dc644bced56e3b6dc296a3344733ccc87de55b99
min-gyeong-00 <als6898@pusan.ac.kr>
2023-01-28 15:53:09 +0900

    start

---
+++ foody/src/test/java/foody/foody/FoodyApplicationTests.java
@@ -0,0 +1,13 @@
+package foody.foody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
